a0ac295b2ccecdf95874653a518ec8891fb3d486
Wyrta <81468617+Wyrta@users.noreply.github.com>
2021-03-27 12:28:52 +0100

    Update header_sb.h

--- src/header_sb.h
+++ src/header_sb.h
@@ -1,12 +1,14 @@
 //inclue tout les fichiers
 
-#include <C:\Users\Tenegan\Desktop\projet_tut/include/SDL/SDL.h>    //librairie SDL2
+#include <C:\Users\****\Desktop\projet_tut/include/SDL/SDL.h>    //librairie SDL2
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <C:\Users\Tenegan\Desktop\projet_tut\src\msgError.c>   //code de le fonction des message d'erreur
-#include <C:\Users\Tenegan\Desktop\projet_tut\src\parametre.c>  //code de le fonction parametre
-#include <C:\Users\Tenegan\Desktop\projet_tut\src\playSound.c>  //code de le fonction pour jouer un son
-#include <C:\Users\Tenegan\Desktop\projet_tut\src\bouton.c>     //pour rafraichire les boutons
-#include <C:\Users\Tenegan\Desktop\projet_tut\src\fichier.c>   //fichier pour tester un l'existance d'un fichier 
-//#include <C:\Users\Tenegan\Desktop\projet_tut\src\test.c>
+
+#include <C:\Users\****\Desktop\projet_tut\src\msgError.c>   //code de le fonction des message d'erreur
+#include <C:\Users\****\Desktop\projet_tut\src\parametre.c>  //code de le fonction parametre
+#include <C:\Users\****\Desktop\projet_tut\src\playSound.c>  //code de le fonction pour jouer un son
+#include <C:\Users\****\Desktop\projet_tut\src\bouton.c>     //pour rafraichire les boutons
+#include <C:\Users\****\Desktop\projet_tut\src\fichier.c>   //fichier pour tester un l'existance d'un fichier 
+//#include <C:\Users\****\Desktop\projet_tut\src\test.c>
